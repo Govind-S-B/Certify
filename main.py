cdd4c29636e97e46b11efaa2f484704afb234baa
Govind-S-B <b.s.dnivog@gmail.com>
2023-07-26 00:01:20 +0530

    added basic screens setup

--- main.py
+++ main.py
@@ -4,19 +4,50 @@ import curses
 client = MongoClient("mongodb://localhost:27017/")
 db = client.certify
 
-def mainScreen(): # View Events
-    collection = db.events
-    items = list(collection.find({},{"event":1,"name":1}))
+def init(stdscr):
+    curses.curs_set(False)
+    stdscr.keypad(True)
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    mainScreen(stdscr,0)
 
-    print("== Certify CLI v1 ==")
-    print("Register New [+]") # Call Register Function , Come Back to main with screen refreshhed
-    print()
+def mainScreen(win,selected_row_idx): # View Events
 
-    if items == None:
-        print("No events registered")
-    else:
-        for i in items:
-            print(f"{i['_id']} {i['name']}" ) # on click call ViewEvent function
+    items = list(db.events.find({},{"_id":1,"name":1}))
+
+    while True:
+
+        win.clear()
+        x , y = 0,0
+        win.addstr(y,x,"== Certify CLI V1 ==",curses.color_pair(1))
+        y+=1
+        win.addstr(y, x,"Quit [q] | Register New [+]",curses.color_pair(1))
+        y+=2
+
+        if items == None:
+            win.addstr("No events registered")
+        else:
+            for idx, item in enumerate(items):
+                if idx == selected_row_idx:
+                    win.attron(curses.color_pair(2))
+                    win.addstr(y, x, f"{item['_id']} {item['name']}")
+                    win.attroff(curses.color_pair(2))
+                else:
+                    win.addstr(y, x, f"{item['_id']} {item['name']}")
+                y += 1
+
+        key = win.getch()
+
+        if key == 81 or key == 113: # for quit
+            break
+        elif key == 43: # for Register new event  # Call Register Function , Come Back to main with screen refreshhed with new List
+            print("add")
+        elif key == curses.KEY_UP and selected_row_idx > 0:
+            selected_row_idx -= 1
+        elif key == curses.KEY_DOWN and selected_row_idx < len(items)-1:
+            selected_row_idx += 1
+        elif key in [curses.KEY_ENTER, 10, 13]: # get the full event details ViewEvent
+            print("go to next screen")
 
 def regEvent(event_id):
     print()
@@ -42,7 +73,7 @@ def ViewParticipant(participant_id):
     print()
 
 # Main Screen Function
-mainScreen()
+curses.wrapper(init)
 
 # In Line Edit Functionality
 # Hover over the line to edit and Ctrl E to edit , allows editing only the editable parts
