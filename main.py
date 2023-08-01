afd45571157f56f64b73b516dc7bba88c02a31b7
Siby C R <sibycr18@gmail.com>
2023-08-01 13:05:39 +0530

    working on addParticipant functionality

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main_screen(win): # View Events
         x , y = 0,0
         win.addstr(y,x,"== Certify CLI V1 ==",curses.color_pair(1))
         y+=1
-        win.addstr(y, x,"Quit [q] | Register New Event [+]",curses.color_pair(1))
+        win.addstr(y, x,"Register New Event [+] | Quit [q]",curses.color_pair(1))
         y+=2
 
         if events_list == []:
@@ -33,20 +33,15 @@ def main_screen(win): # View Events
         else:
             for idx, item in enumerate(events_list):
                 if idx == selected_row_idx:
-                    win.attron(curses.color_pair(2))
-                    win.addstr(y, x, f"{item['_id']} {item['name']}")
-                    win.attroff(curses.color_pair(2))
+                    win.addstr(y, x, f"{item['_id']} {item['name']}", curses.color_pair(2))
                 else:
                     if item["issueDt"] == None: # check finalized
-                        win.attron(curses.color_pair(3))
-                        win.addstr(y, x, f"{item['_id']} {item['name']}")
-                        win.attroff(curses.color_pair(3))
+                        win.addstr(y, x, f"{item['_id']} {item['name']}", curses.color_pair(3))
                     else:
                         win.addstr(y, x, f"{item['_id']} {item['name']}")
                 y += 1
 
         key = win.getch()
-
         if key == 81 or key == 113: # Quit
             break
         elif key == 43: # Register
@@ -114,21 +109,88 @@ def view_event(win,event_id):
     win.addstr(y,x,f"Participant Fields : {item['fields']}",curses.color_pair(1))
     y+=2
 
-    win.addstr(y,x,f"Show Participants",curses.color_pair(1))
+    win.addstr(y,x,f"Show Participants [S]",curses.color_pair(3))
     y+=1
 
-    win.getch()
+    key = win.getch()
+    if key == 81 or key == 113: # Quit
+        return
+    elif key in [curses.KEY_ENTER, 10, 13]:
+        viewParticipant(win, event_id, finalized)
     
     # viewParticipants(event_id,finalized)
 
-def viewParticipants(event_id,finalized):
+def viewParticipants(win, event_id, finalized):
     # Add Participant [+]
     # Add Via CSV [~]
     # ... Participant List // View Participant with inline value edit functionality
-    raise NotImplementedError
 
-def addParticipantCLI():
-    raise NotImplementedError
+    # participants_list = list(db.participants.find({},{"_id":1,"name":1,"issueDt":1}))
+    participants_list = []
+    selected_row_idx = 0 # initially select index
+
+    while True:
+        win.clear()
+        x , y = 0,0
+        win.addstr(y, x,"Add Participant [+] | Add Via CSV [~]",curses.color_pair(1))
+        y+=2
+        if participants_list == []:
+            win.addstr(y,x,"No participants added")
+            y+=1
+        else:
+            for idx, item in enumerate(participants_list):
+                if idx == selected_row_idx:
+                    win.addstr(y, x, f"{item['_id']} {item['name']}", curses.color_pair(2))
+                else:
+                    if finalized == False: # check finalized
+                        win.addstr(y, x, f"{item['_id']} {item['name']}", curses.color_pair(3))
+                    else:
+                        win.addstr(y, x, f"{item['_id']} {item['name']}")
+                y += 1
+        
+
+        key = win.getch()
+        if key == 81 or key == 113: # Quit
+            return
+        elif key == 43: # Register
+            addParticipantCLI(win, event_id)
+            # participants_list = list(db.events.find({},{"_id":1,"name":1,"issueDt":1}))
+        # elif key == curses.KEY_UP and selected_row_idx > 0:
+        #     selected_row_idx -= 1
+        # elif key == curses.KEY_DOWN and selected_row_idx < len(events_list)-1:
+        #     selected_row_idx += 1
+        # elif key in [curses.KEY_ENTER, 10, 13]: # View Event
+        #     view_event(win,events_list[selected_row_idx]["_id"])
+        #     events_list = list(db.events.find({},{"_id":1,"name":1,"issueDt":1}))
+        else:
+            continue
+
+    # raise NotImplementedError
+
+def addParticipantCLI(win, event_id):
+    curses.curs_set(True)
+    curses.echo()
+    win.clear()
+    x,y = 0,0
+
+    item = {"name":None,"desc":None,"issueDt":None,"fields":["_id","name"]}
+
+    win.addstr(y,x,"Event Name : ",curses.color_pair(1))
+    item["name"] = win.getstr().decode("utf-8") 
+    y+=1
+    win.addstr(y,x,"Description : ",curses.color_pair(1))
+    item["desc"] = win.getstr().decode("utf-8")
+    y+=1
+
+    win.addstr(y,x,"Fields : ",curses.color_pair(1))
+    item["fields"].extend(win.getstr().decode("utf-8").split())
+    y+=1
+
+    db.participants.insert_one(item)
+
+    curses.noecho()
+    curses.curs_set(False)
+
 
 def addParticipantCSV():
     # https://docs.python.org/3/library/csv.html#csv.DictReader
