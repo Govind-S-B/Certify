c186f895fea798855c7c4c24618c0ff76972bb2c
Govind-S-B <b.s.dnivog@gmail.com>
2023-08-05 00:05:39 +0530

    an unsatisfactory fix for the flask 2.2.5 to 2.3.2 bug

--- api_server.py
+++ api_server.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask, make_response
 from bson import ObjectId
 from pymongo import MongoClient
 import json
@@ -13,7 +13,6 @@ class CustomJSONEncoder(json.JSONEncoder):
         return super().default(obj)
 
 app = Flask(__name__)
-app.json_encoder = CustomJSONEncoder
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client.certify
@@ -21,7 +20,10 @@ db = client.certify
 @app.route('/active', methods=['GET'])
 def get_active_status():
     response = {'active': True}
-    return response
+    
+    r = make_response(json.dumps(response, cls=CustomJSONEncoder))
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
 @app.route("/getgeninfo/<event_id>")
 def get_gen_info(event_id):
@@ -36,7 +38,9 @@ def get_gen_info(event_id):
         "participants": participants
     }
 
-    return jsonify(response)
+    r = make_response(json.dumps(response, cls=CustomJSONEncoder))
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6969)
